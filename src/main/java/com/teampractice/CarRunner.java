cd9a330c7a49165a12be2aa5dc5abde70dcb96b3
monkIO <ivanozernuk@gmail.com>
2022-10-02 13:44:05 +0300

    CarRunner create 2

---
+++ src/main/java/com/teampractice/CarRunner.java
@@ -0,0 +1,10 @@
+package com.teampractice;
+
+import java.util.Date;
+
+public class CarRunner {
+
+    public static void main(String[] args){
+        //Car[] carArray = {new Car("Honda", "Civic", Date.parse("2008-01-01"), Color.WHITE, 7000), new Car()};
+    }
+}
